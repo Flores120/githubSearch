e2a08244e61547297e391ad85b5b88dd22941b56
Alvaro Flores <alvaro.flores01@gmail.com>
2016-10-14 10:28:54 -0700

    added my apiKey

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var apiKey = "b39207005a23e54f016a20d706ad10f551e49024";
 
 $(document).ready(function(){
   $('#search').submit(function(event){
     event.preventDefault();
     var search = $('#searchInput').val();
+    $.get('https://api.github.com/users?b39207005a23e54f016a20d706ad10f551e49024=' + apiKey).then(function(response){
+    console.log(response);
+  });
     $('.results').text("Here are your results " + search);
   });
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,8 +1,12 @@
+var apiKey = "b39207005a23e54f016a20d706ad10f551e49024";
 
 $(document).ready(function(){
   $('#search').submit(function(event){
     event.preventDefault();
     var search = $('#searchInput').val();
+    $.get('https://api.github.com/users?b39207005a23e54f016a20d706ad10f551e49024=' + apiKey).then(function(response){
+    console.log(response);
+  });
     $('.results').text("Here are your results " + search);
   });
 });
