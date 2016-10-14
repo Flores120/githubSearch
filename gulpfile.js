18b3298bf88e51ea5365842dbe057620baaea6da
Alvaro Flores <alvaro.flores01@gmail.com>
2016-10-14 10:06:25 -0700

    added the server

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,15 @@ var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
 var del = require('del');
 var jshint = require('gulp-jshint');
-
+var browserSync = require('browser-sync').create();
+// var lib = require('bower-files')();
+//
+// gulp.task('bowerJS', function () {
+//   return gulp.src(lib.ext('js').files)
+//     .pipe(concat('vendor.min.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('./build/js'));
+// });
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -43,6 +51,22 @@ gulp.task("build", ['clean'], function(){
   } else {
     gulp.start('jsBrowserify');
   }
+});
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
